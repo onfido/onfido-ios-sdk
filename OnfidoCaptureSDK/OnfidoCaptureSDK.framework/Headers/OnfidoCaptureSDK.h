e37a0e6145d828985c08430b4b0e696cfd0c5cd5
Tiago Pinto <tiago.pinto@onfido.com>
2016-07-20 15:03:31 +0100

    Resources in a dir

---
+++ OnfidoCaptureSDK/OnfidoCaptureSDK.framework/Headers/OnfidoCaptureSDK.h
@@ -0,0 +1,17 @@
+//
+//  OnfidoCaptureSDK.h
+//  OnfidoCaptureSDK
+//
+//  Created by Tiago Pinto on 09/06/2016.
+//  Copyright © 2016 Onfido. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OnfidoCaptureSDK.
+FOUNDATION_EXPORT double OnfidoCaptureSDKVersionNumber;
+
+//! Project version string for OnfidoCaptureSDK.
+FOUNDATION_EXPORT const unsigned char OnfidoCaptureSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnfidoCaptureSDK/PublicHeader.h>
