ae99607d3b40c947557079429b14f05943bddf57
Anurag Ajwani <anurag.ajwani@onfido.com>
2021-02-01 11:25:28 +0000

    Fixed SPM link

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Onfido",
-            url: "https://s3-eu-west-1.amazonaws.com/ios/Onfido-v20.1.0.zip",
+            url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/Onfido-v20.1.0.zip",
             checksum: "34b6d04053f41275e432c17f61ad7c32739e6980ebca91e38e967ccbdf6c71c4"
         )
     ]
