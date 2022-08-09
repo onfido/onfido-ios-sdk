7f78729bfb036de610e5974f13746c3599e04908
Antonio Casero <antonio.casero@onfido.com>
2022-08-09 10:00:25 +0200

    changes for 25.2.0 release

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "Onfido",
             url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/Onfido-v25.2.0.zip",
-            checksum: "03f6cb00cba2bab02f8d5a7c670a346d160e82787298b41010ddaff9f340b8d3"
+            checksum: "64f2f5ffe4902811cae4f0d0428db0ec4a9cbca6fb65b7b34c150003130d2764"
         )
     ]
 )
