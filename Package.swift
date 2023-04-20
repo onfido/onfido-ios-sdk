d08195b6ea7696ef54490ceee38ac50ec96236cb
Kerem Gunduz <kerem.gunduz@onfido.com>
2023-04-20 17:37:11 +0100

    changes for 25.0.1 release

--- Package.swift
+++ Package.swift
@@ -1,37 +1,24 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+
 let package = Package(
     name: "Onfido",
     platforms: [
         .iOS(.v11)
     ],
     products: [
-        
         .library(
             name: "Onfido",
-            targets: ["Onfido"]),
-        
-        .library(
-            name: "OnfidoExtended",
-            targets: ["OnfidoExtended"]),
-        
+            targets: ["Onfido"])
+    ],
+    dependencies: [
     ],
     targets: [
-        
-            .binaryTarget(
-                name: "Onfido",
-                url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/Onfido-v28.3.0.zip",
-                checksum: "a8a68ddf9af3ca9a39df842ba65b1d819d952203337e73cba7c6906268c5d3e1"
-            ),
-        
-            .binaryTarget(
-                name: "OnfidoExtended",
-                url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/OnfidoExtended-v28.3.0.zip",
-                checksum: "e85e0df562150aedbf9fa0a1236d8060a7a47abf75fb51af2f82bdf7ddd0c131"
-            ),
-        
-
-        
+        .binaryTarget(
+            name: "Onfido",
+            url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/Onfido-v25.0.1.zip",
+            checksum: "90abc69deff54910790bdecf969a58be3c7d1525f2c1bf3c9b2127077a7951cc"
+        )
     ]
 )
--- SampleAppObjC/SampleAppObjC/TestFile.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TestFile.swift
-//
-//  Copyright © 2016-2023 Onfido. All rights reserved.
-//
-
-import Foundation
-
-// required
