3ec985ef8aa4ce66339c002b0b0b74dffe054e09
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-04-29 13:53:43 +0000

    changes for 30.0.1 release

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         
             .binaryTarget(
                 name: "Onfido",
-                url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/Onfido-v30.0.0.zip",
-                checksum: "6a4f9f6ea755bb8c66aecd2835fc61458fcf2c363bfcad4d313ad61f05b807de"
+                url: "https://s3-eu-west-1.amazonaws.com/onfido-sdks/ios/Onfido-v30.0.1.zip",
+                checksum: "445076f647e892168af0617345f2621ed7c6d60e17e580561cf36272aba393d9"
             ),
         
 
